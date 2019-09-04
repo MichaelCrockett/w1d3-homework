3b3c34601d15d79ef15e327e22dd0edbfdf0e338
Michael Crockett <mike.crockett76@gmail.com>
2019-09-05 00:03:22 +0100

    final commit of homework w1d3 - incomplete

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -68,8 +68,30 @@ p users["Erik"][:lottery_numbers]
 p users["Avril"][:pets][0][:species]
 
 # 5. Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].sort.first
+
 # 6. Return an array of Avril's lottery numbers that are even
+numbers = users["Avril"][:lottery_numbers]
+numbers.each do |evens|
+  if evens.to_i % 2 == 0
+p evens #prints Avril's even lottery_numbers but i gave up trying to get them into an array! Tried lots of ways but deleted them.
+end
+end
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"][:lottery_numbers].push(7)
+p users["Erik"][:lottery_numbers]
+
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = ("Edinburgh")
+p users["Erik"][:home_town]
+
 # 9. Add a pet dog to Erik called "Fluffy"
+# users["Erik"][:pets][:name] << ("Fluffy")[:species] << ("dog")
+#I know this is a simple fix, but I can't get it
+
+
 # 10. Add another person to the users hash
+# users << "Mike"
+# puts users
+#again, I know this is a simple solution but I can't get it
